a83d69d7cb5d7f39c2c0590e4ac0091b6dfd5c14
Vedant Deshmukh <deshmukhvedant95@gmail.com>
2026-03-31 20:00:07 +0530

    chore: bump version to 0.1.1rc2 for Test PyPI release

--- openlmlib/__init__.py
+++ openlmlib/__init__.py
@@ -3,7 +3,7 @@
 from .settings import Settings, load_settings
 from .schema import Finding, FindingAudit, FindingText, ValidationIssue
 
-__version__ = "0.1.0"
+__version__ = "0.1.1rc2"
 
 __all__ = [
     "__version__",
